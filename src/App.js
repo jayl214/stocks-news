1bb1fbe60052b0c2346e1d14c39d6891e68178e7
jayl214 <jay.lin@mail.mcgill.ca>
2018-07-13 22:51:24 -0400

    calls iex api for all company names and tickers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Searchbar from './Searchbar.jsx'
+import axios from 'axios'
 import './App.css';
 
 class App extends Component {
-  render() {
+
+  constructor = (props) => {
+    super(props)
+
+    this.state = {
+      tickersAndNames: [],
+      searchBarInput: "",
+      searchbarSuggestions: [],
+    }
+
+  }
+
+  componentDidMount = () => {
+    this.getTickersAndNames()
+  }
+
+  getTickersAndNames = () => {
+    axios.get('https://api.iextrading.com/1.0/ref-data/symbols')
+      .then( (response) => {
+        this.state.tickersAndNames = response
+        console.log(this.state.tickersAndNames);
+      })
+      .catch( (error) => {
+        // handle error
+        console.log(error);
+      })
+      .then( () =>{
+        // always executed
+      })
+  }
+
+  searchbarSearch = (event) => {
+    const input = event.target.value
+    this.state.searchBarInput = input
+    console.log(this.state.searchBarInput)
+  }
+
+
+  render = () => {
     return (
       <div className="App">
         <header className="App-header">
@@ -13,8 +53,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Searchbar searchbarSearch = {this.searchbarSearch} searchbarSuggestions = {this.state.searchbarSuggestions} />
       </div>
-    );
+    )
   }
 }
 
---
+++ src/Searchbar.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Suggestion from './Suggestion.jsx'
+
+class Searchbar extends Component {
+
+  constructor = (props) => {
+    super(props)
+  }
+
+  render = () => {
+    return (
+
+      <div>
+        <input type="text" placeholder="Company" onKeyUp = {this.props.searchbarSearch} />
+        <ul>
+          {this.props.searchbarSuggestions.map((suggestion, index)=>{
+            return <Suggestion suggestion = {suggestion} key = {index}  />
+          }) }
+        </ul>
+      </div>
+
+    );
+  }
+
+}
+
+export default Searchbar;
